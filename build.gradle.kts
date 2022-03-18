d38133edb806ec321c47188d27cd0da45e5b1618
Steve Soltys <steve@stevesoltys.com>
2022-03-17 22:18:11 -0400

    Fix local Maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     kotlin("jvm") version "1.5.10"
     id("com.apollographql.apollo3").version("3.1.0")
     `maven-publish`
-    java
 }
 
 group = "com.stevesoltys.aidungeon"
@@ -20,4 +19,12 @@ dependencies {
 
 apollo {
     packageName.set("com.stevesoltys.aidungeon")
+}
+
+publishing {
+    publications {
+        val mavenJava by creating(MavenPublication::class) {
+            from(components["kotlin"])
+        }
+    }
 }
